02039ba6a5f1374c3dbee9bead86a40737e456c2
chalmery <chaochaoycc@gmail.com>
2023-07-01 17:12:58 +0800

    style💄: 调整本地音乐文件夹列表展示样式

--- src/compontes/center/Local.jsx
+++ src/compontes/center/Local.jsx
@@ -1,173 +1,182 @@
-import React, {useEffect, useRef, useState} from 'react'
-import {Layout, Menu, Table} from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Layout, Menu, Table } from "antd";
 import dataEvent from "../../../electron/lib/event";
 import localSetting from "../../../electron/lib/event";
-import eventManager from '../../event/eventManager';
+import eventManager from "../../event/eventManager";
 import pageEvent from "@/event/pageEvent";
-import {playModeRepository} from "@/strategy/repository/repository";
-import icon from '/icons/music256x256.png';
+import { playModeRepository } from "@/strategy/repository/repository";
 
-const {Content, Sider} = Layout;
-
+const { Content, Sider } = Layout;
 
 export default function Local() {
-    const columns = [
-        {
-            "title": "图片", "dataIndex": "picture", "key": "picture",
-            render: (picture) => {
-                let img = picture === null ? icon : `file://${picture}`
-                return <img src={img} alt={icon} style={{width: '35px', borderRadius: "2px"}}/>
-            }
-        },
-        {"title": "歌曲", "dataIndex": "title", "key": "title"},
-        {"title": "歌手", "dataIndex": "artist", "key": "artist"},
-        {"title": "专辑", "dataIndex": "album", "key": "album"},
-        {"title": "时长", "dataIndex": "duration", "key": "duration"},
-        {"title": "类型", "dataIndex": "type", "key": "type"}
-    ];
+  const columns = [
+    { title: "歌曲", dataIndex: "title", key: "title" },
+    { title: "歌手", dataIndex: "artist", key: "artist" },
+    { title: "专辑", dataIndex: "album", key: "album" },
+    { title: "时长", dataIndex: "duration", key: "duration" },
+    { title: "类型", dataIndex: "type", key: "type" },
+  ];
 
-    const [dirList, setDirList] = useState([]);
-    const [dataSource, setDataSource] = useState([]);
-    const dirListRef = useRef(dirList);
-    const [thisMusic, setThisMusic] = useState(null);
+  const dirColumns = [
+    {
+      title: "文件夹",
+      dataIndex: "label",
+      key: "label",
+    },
+  ];
 
-    useEffect(() => {
-        //查数据
-        electron.ipcRenderer.send(dataEvent.LOCAL.value, dataEvent.LOCAL.value)
+  const [dirList, setDirList] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
+  const dirListRef = useRef(dirList);
+  const [thisMusic, setThisMusic] = useState(null);
+  const [thisDir, setThisDir] = useState(null);
 
-        //回调
-        electron.ipcRenderer.on(dataEvent.LOCAL_CALLBACK.value, (event, data) => {
-            setDirList(data)
-            dirListRef.current = data
-        });
+  useEffect(() => {
+    //查数据
+    electron.ipcRenderer.send(dataEvent.LOCAL.value, dataEvent.LOCAL.value);
 
+    //回调
+    electron.ipcRenderer.on(dataEvent.LOCAL_CALLBACK.value, (event, data) => {
+      setDirList(data);
+      dirListRef.current = data;
+    });
 
-        //下一首
-        eventManager.subscribe(pageEvent.NEXT.value, handleNext);
+    //下一首
+    eventManager.subscribe(pageEvent.NEXT.value, handleNext);
 
-        //上一首
-        eventManager.subscribe(pageEvent.PRE.value, handlePre);
+    //上一首
+    eventManager.subscribe(pageEvent.PRE.value, handlePre);
 
-        // 取消订阅
-        return () => {
-            electron.ipcRenderer.removeAllListeners(localSetting.LOCAL_CALLBACK.value);
-            eventManager.unsubscribe(pageEvent.NEXT.value, handleNext);
-            eventManager.unsubscribe(pageEvent.PRE.value, handlePre);
-        };
-
-    }, []);
-
-
-    const handleMenuClick = ({key}) => {
-        const selectedItem = dirList.find((item) => item.key === key);
-        if (selectedItem) {
-            const {value} = selectedItem;
-            setDataSource(value)
-        }
-    }
-
-
-    /**
-     * 上一首事件处理
-     * @param data 当前音乐元数据
-     */
-    const handlePre = (data) => {
-        //策略
-        let {playMode} = data
-        let fun = playModeRepository.get(playMode)
-        const newData = {
-            ...data,
-            dirList: dirListRef.current,
-            type: pageEvent.PRE,
-            call: setThisMusic
-        }
-        fun(newData)
-    }
-
-    /**
-     * 下一首事件处理
-     * @param data 当前音乐元数据
-     */
-    const handleNext = (data) => {
-        //策略
-        let {playMode} = data
-        let fun = playModeRepository.get(playMode)
-        const newData = {
-            ...data,
-            dirList: dirListRef.current,
-            type: pageEvent.NEXT,
-            call: setThisMusic
-        }
-        fun(newData)
-    }
-
-    /**
-     * 查询歌词
-     * @param data
-     */
-    const findLrc = (data) => {
-        electron.ipcRenderer.send(dataEvent.LRC.value, data)
-    }
-
-    /**
-     * 列表点击
-     * @param record
-     */
-    function tableRowClick(record) {
-        setThisMusic(record)
-        eventManager.publish(pageEvent.CLICK_MUSIC.value, record)
-        findLrc(record)
-    }
-
-
-    const rowClassName = (record, index) => {
-        if (thisMusic) {
-            return thisMusic === record ? 'color-row' : ''
-        }
-        return ''
+    // 取消订阅
+    return () => {
+      electron.ipcRenderer.removeAllListeners(localSetting.LOCAL_CALLBACK.value);
+      eventManager.unsubscribe(pageEvent.NEXT.value, handleNext);
+      eventManager.unsubscribe(pageEvent.PRE.value, handlePre);
     };
+  }, []);
 
-    return (
-      <Layout className='layout'>
-          <Sider className='scrollable-container auto'>
-              {dirList.length > 0 && (<>
-                    <Menu
-                      className='border0'
-                      theme="light"
-                      mode="inline"
-                      items={dirList}
-                      onClick={handleMenuClick}
-                    >
-                    </Menu>
-                    <div style={{height: "55px"}}></div>
-                </>
-                )}
-            </Sider>
+  /**
+   * 上一首事件处理
+   * @param data 当前音乐元数据
+   */
+  const handlePre = (data) => {
+    //策略
+    let { playMode } = data;
+    let fun = playModeRepository.get(playMode);
+    const newData = {
+      ...data,
+      dirList: dirListRef.current,
+      type: pageEvent.PRE,
+      call: setThisMusic,
+    };
+    fun(newData);
+  };
 
-            <Content className='scrollable-container auto heightMax'>
-                {dataSource.length > 0 && (<>
-                      <Table
-                        onRow={(record) => {
-                            return {
-                                onClick: (event) => {
-                                    tableRowClick(record);
-                                }
-                            };
-                        }}
-                        rowClassName={rowClassName}
-                        size={"small"}
-                        columns={columns}
-                        pagination={false}
-                        dataSource={dataSource}
-                      />
-                      <div style={{height: "55px"}}></div>
-                    </>
-                )}
+  /**
+   * 下一首事件处理
+   * @param data 当前音乐元数据
+   */
+  const handleNext = (data) => {
+    //策略
+    let { playMode } = data;
+    let fun = playModeRepository.get(playMode);
+    const newData = {
+      ...data,
+      dirList: dirListRef.current,
+      type: pageEvent.NEXT,
+      call: setThisMusic,
+    };
+    fun(newData);
+  };
 
-            </Content>
+  /**
+   * 查询歌词
+   * @param data
+   */
+  const findLrc = (data) => {
+    electron.ipcRenderer.send(dataEvent.LRC.value, data);
+  };
 
+  /**
+   * 列表点击
+   * @param record
+   */
+  function tableRowClick(record) {
+    setThisMusic(record);
+    eventManager.publish(pageEvent.CLICK_MUSIC.value, record);
+    findLrc(record);
+  }
 
-        </Layout>
+  //文件夹列表点击
+  const handleMenuClick = (record) => {
+    setThisDir(record);
+    const selectedItem = dirList.find((item) => item.key === record.key);
+    if (selectedItem) {
+      const { value } = selectedItem;
+      setDataSource(value);
+    }
+  };
 
-    )
+  const rowClassName = (record, index) => {
+    if (thisMusic) {
+      return thisMusic === record ? "color-row" : "";
+    }
+    return "";
+  };
+
+  const dirRowClassName = (record, index) => {
+    if (thisDir) {
+      return thisDir === record ? "color-row" : "";
+    }
+    return "";
+  };
+
+  return (
+    <Layout className="layout">
+      <Sider className="scrollable-container auto">
+        {dirList.length > 0 && (
+          <>
+            <Table
+              onRow={(record) => {
+                return {
+                  onClick: () => {
+                    handleMenuClick(record);
+                  },
+                };
+              }}
+              size={"small"}
+              rowClassName={dirRowClassName}
+              showHeader={false}
+              dataSource={dirList}
+              columns={dirColumns}
+              pagination={false}
+            />
+            <div style={{ height: "55px" }}></div>
+          </>
+        )}
+      </Sider>
+
+      <Content className="scrollable-container auto heightMax">
+        {dataSource.length > 0 && (
+          <>
+            <Table
+              onRow={(record) => {
+                return {
+                  onClick: () => {
+                    tableRowClick(record);
+                  },
+                };
+              }}
+              rowClassName={rowClassName}
+              size={"small"}
+              columns={columns}
+              pagination={false}
+              dataSource={dataSource}
+            />
+            <div style={{ height: "55px" }}></div>
+          </>
+        )}
+      </Content>
+    </Layout>
+  );
 }
